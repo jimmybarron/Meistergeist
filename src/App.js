379e66391649e17a47a028b2f6260f130cd9cbd3
Jimmy Barron <jimmy.r.barron@gmail.com>
2022-04-20 20:27:08 -0700

    Outlined basic game logic approach

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "./App.css";
 
 function App() {
+  // Get secret code
+  // Get user guess attempt
+  // Iterate through guess. Check for matching character, and matching position.
+  // If number is contained in guess, log 'numMatch' and 'numAndPosMatch' for both, to guessSummary object
+  // If 'numAndPosMatch' === 4; player wins, If 'numAndPosMatch' < 4 and 'guessAttempts' <= 9; player loses.
+
   return <div className="App">Mastermind</div>;
 }
 
