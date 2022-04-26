92bb52ba437377ba2e77d22bb3b99ae140a2da9a
Jimmy Barron <jimmy.r.barron@gmail.com>
2022-04-26 11:51:27 -0700

    Created fetch for random code

--- src/App.js
+++ src/App.js
@@ -4,10 +4,37 @@ import GuessAttempts from "./GuessAttempts.js";
 import { useEffect, useState } from "react";
 
 function App() {
-  // Get secret code
-  let secretCode = "3409";
+  const [secretCode, setSecretCode] = useState();
   const [win, setWin] = useState();
   const [guesses, setGuesses] = useState([]);
+
+  useEffect(() => {
+    // Get secret code
+    let getSecretCode = async () => {
+      await fetch(
+        "https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&rnd=new&format=plain",
+        {
+          method: "GET",
+          mode: "cors",
+        }
+      )
+        // Wait for the fetch to resolve
+        .then((resp) => {
+          // wait for the text method to resolve to a string
+          resp.text().then((resp) => {
+            setSecretCode(resp.replace(/\n/g, ""));
+          });
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    };
+    getSecretCode();
+  }, []);
+
+  useEffect(() => {
+    console.log(secretCode);
+  }, [secretCode]);
 
   // Check for losing conditions
   useEffect(() => {
